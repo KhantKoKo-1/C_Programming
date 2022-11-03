2ed5f76f14f2787fa767a2485ca61f244e4f30da
KhantOnlineNcc <117338315+KhantOnline7012@users.noreply.github.com>
2022-11-03 13:29:07 +0630

    Create 2userInput.c

---
+++ 2userInput.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+int main(){
+    int y=0;
+    printf("please enter the number of y: ");
+    scanf("%d",&y);
+
+    int x=0;
+    printf("please enter the the number of x: ");
+    scanf("%d",&x);
+
+    int sum=x+y;
+    printf("sum is:%d",sum);
+
+
+    return 0;
+}
