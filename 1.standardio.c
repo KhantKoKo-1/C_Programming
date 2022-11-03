e9762fbcd549ba43082ff315ca8818f45c6f1ce4
KhantOnlineNcc <117338315+KhantOnline7012@users.noreply.github.com>
2022-11-03 12:45:18 +0630

    Create 1.standardio.c

---
+++ 1.standardio.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {//() parenthesis
+    //{} curly brackets
+    //Standard Output
+    printf("Hello World\n");
+    printf("Hello I am Khant KO KO!!!\n");
+
+
+    //Standard Input
+    int no;
+    printf("Enter your no");
+    scanf("%d",&no);//& ---> address of
+    printf("Your no is: %d",no);
+    return 0;
+}
