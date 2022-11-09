fa926b33a5cc4ebf79996de996742d5fd0acff90
KhantOnlineNcc <117338315+KhantOnline7012@users.noreply.github.com>
2022-11-09 20:44:18 +0630

    Create KKKbankingearlystate.c

---
+++ KKKbankingearlystate.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main(){
+int account= 1;
+int ph = 1234;
+int pw=0;
+int otg=0;
+
+
+    printf("****KKK BANKING*****\n");
+    printf("*Welcome user*\nDo you have account?\nYes(1) or NO (2):");
+    scanf("%d",&account);
+
+if(account == 1) {
+    while (ph != 12345) {
+        printf("please enter your Ph number =");
+
+        scanf("%d", &ph);}
+
+
+        while(pw!=2222){
+        printf("Enter your password\n",pw);
+        scanf("%d",&pw);
+
+    }
+}
+else
+    while (ph != 12345) {
+
+    printf("please sign up with your ph no.",ph);
+
+        scanf("%d", &ph);
+        printf("your OtG code is 2222\n",otg);
+
+    }
+
+    if(pw==2222){
+
+    printf("welcom member \n",pw);
+
+    }else;
+
+
+
+
+
+
+
+
+
+        return 0;}
+
